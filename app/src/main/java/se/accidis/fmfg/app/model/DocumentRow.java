83b305d933e8e34f52068b0554ab8f9efd791f18
Wilhelm Svenselius <wilhelm.svenselius@gmail.com>
2015-10-16 00:06:58 +0200

    Datamodell.

--- app/src/main/java/se/accidis/fmfg/app/model/DocumentRow.java
+++ app/src/main/java/se/accidis/fmfg/app/model/DocumentRow.java
@@ -1,22 +1,54 @@
 package se.accidis.fmfg.app.model;
 
-import java.util.List;
+import java.math.BigDecimal;
 
 /**
  * Model object for rows in a transport declaration.
  */
 public final class DocumentRow {
-    private String mFben; // Förrådsbenämning
-    private String mUNnr; // UN-nummer
-    private String mNamn; // Namn
-    private List<String> mKlassKod; // Klassificeringskod/etiketter
-    private int mNEMmg; // NEM i mg (per enhet)
-    private int mNEMEnheter; // Antal enheter för beräkning av NEM
-    private int mAntalKolli; // Antal kolli
-    private String mTypKolli; // Beskrivning av kolli
-    private int mTpKat; // Transportkategori
-    private String mFrpGrp; // Förpackningsgrupp
-    private String mTunnelkod; // Tunnelkod
-    private boolean mMiljo; // Miljöfarligt
-    private int mKvantitet; // Kvantitet fg i ml eller mg
+    private final Material mMaterial;
+    private int mAmount; // Antal enheter för beräkning av NEM
+    private int mNumberOfPkgs; // Antal kolli
+    private String mTypeOfPkgs; // Beskrivning av kolli
+    private BigDecimal mWeightVolume; // Kvantitet farligt gods i liter / kg
+
+    public DocumentRow(Material material) {
+        mMaterial = material;
+    }
+
+    public int getAmount() {
+        return mAmount;
+    }
+
+    public void setAmount(int amount) {
+        mAmount = amount;
+    }
+
+    public Material getMaterial() {
+        return mMaterial;
+    }
+
+    public int getNumberOfPackages() {
+        return mNumberOfPkgs;
+    }
+
+    public void setNumberOfPackages(int numPackages) {
+        mNumberOfPkgs = numPackages;
+    }
+
+    public String getTypeOfPackages() {
+        return mTypeOfPkgs;
+    }
+
+    public void setTypeOfPackages(String typeOfPackages) {
+        mTypeOfPkgs = typeOfPackages;
+    }
+
+    public BigDecimal getWeightVolume() {
+        return mWeightVolume;
+    }
+
+    public void setWeightVolume(BigDecimal weightVolume) {
+        mWeightVolume = weightVolume;
+    }
 }
