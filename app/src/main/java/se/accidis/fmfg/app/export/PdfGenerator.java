dd4cfabc7119aa28fba9e1a68bd49acfe8775139
Wilhelm Svenselius <wilhelm.svenselius@chas.se>
2016-02-09 18:39:51 +0100

    PDF-export, sidlayout och formatering.

--- app/src/main/java/se/accidis/fmfg/app/export/PdfGenerator.java
+++ app/src/main/java/se/accidis/fmfg/app/export/PdfGenerator.java
@@ -11,6 +11,7 @@ import com.pdfjet.Font;
 import com.pdfjet.Line;
 import com.pdfjet.PDF;
 import com.pdfjet.Page;
+import com.pdfjet.Point;
 import com.pdfjet.Table;
 import com.pdfjet.TextBlock;
 import com.pdfjet.TextLine;
@@ -36,8 +37,10 @@ public final class PdfGenerator {
 	private static final float mAddressBlockMinHeight = 60.0f;
 	private static final float mHorizontalMargin = 40.0f;
 	private static final float mInnerMargin = 5.0f;
-	private static final float mPageWidth = A4.PORTRAIT[0] - mHorizontalMargin * 2.0f;
-	private static final float mVanityBottomMargin = 30.0f;
+	private static final float mPageHeight = A4.PORTRAIT[1];
+	private static final float mPageWidth = A4.PORTRAIT[0];
+	private static final float mContentWidth = mPageWidth - mHorizontalMargin * 2.0f;
+	private static final float mFooterBottomMargin = 30.0f;
 	private static final float mVerticalMargin = 50.0f;
 	private final Context mContext;
 	private final Document mDocument;
@@ -115,27 +118,23 @@ public final class PdfGenerator {
 	private void createTableHeader(List<List<Cell>> rows) {
 		List<Cell> row = new ArrayList<>(3);
 
-		Cell materialHeaderCell = new Cell(mLabelFont, "Transportmärkning");
+		Cell materialHeaderCell = new Cell(mLabelFont, mContext.getString(R.string.document_export_material_header));
 		materialHeaderCell.setLeftPadding(0);
 		row.add(materialHeaderCell);
 
-		Cell packagesHeaderCell = new Cell(mLabelFont, "Kollin");
+		Cell packagesHeaderCell = new Cell(mLabelFont, mContext.getString(R.string.document_export_pkgs_header));
 		row.add(packagesHeaderCell);
 
-		Cell weightVolumeHeaderCell = new Cell(mLabelFont, "Vikt/volym");
+		Cell weightVolumeHeaderCell = new Cell(mLabelFont, mContext.getString(R.string.document_export_weight_volume_header));
 		row.add(weightVolumeHeaderCell);
 
 		rows.add(row);
 	}
 
 	private void writeDocument() throws Exception {
-		float centerOfPage = mHorizontalMargin + mPageWidth / 2.0f;
+		float centerOfPage = mHorizontalMargin + mContentWidth / 2.0f;
 
 		Page page = new Page(mPdf, A4.PORTRAIT);
-		TextLine vanityLabel = new TextLine(mVanityFont, String.format(mContext.getString(R.string.document_export_vanity_format), mContext.getString(R.string.app_name), AndroidUtils.getAppVersionName(mContext)));
-		vanityLabel.setLocation(mHorizontalMargin, A4.PORTRAIT[1] - mVanityBottomMargin);
-		vanityLabel.drawOn(page);
-
 		final float headerBottom = writeDocumentHeader(page, centerOfPage);
 
 		Table table = new Table();
@@ -144,21 +143,45 @@ public final class PdfGenerator {
 		table.setBottomMargin(mVerticalMargin);
 		table.setNoCellBorders();
 
-		table.setColumnWidth(0, mPageWidth * 0.7f);
-		table.setColumnWidth(1, mPageWidth * 0.15f);
-		table.setColumnWidth(2, mPageWidth * 0.15f);
+		table.setColumnWidth(0, mContentWidth * 0.7f);
+		table.setColumnWidth(1, mContentWidth * 0.15f);
+		table.setColumnWidth(2, mContentWidth * 0.15f);
 		table.wrapAroundCellText();
 
+		int pageNo = 1;
+		final int pageTotal = table.getNumberOfPages(page);
+		Point tableBottom;
+
+		TextLine vanityLabel = new TextLine(mVanityFont, String.format(mContext.getString(R.string.document_export_vanity_format), mContext.getString(R.string.app_name), AndroidUtils.getAppVersionName(mContext)));
+		vanityLabel.setLocation(mHorizontalMargin, mPageHeight - mFooterBottomMargin);
+		vanityLabel.drawOn(page);
+
+		TextLine pageLabel = new TextLine(mTextFont);
+		writePageLabel(page, pageLabel, pageNo, pageTotal);
+
 		while (true) {
-			table.drawOn(page);
+			tableBottom = table.drawOn(page);
 			if (!table.hasMoreData()) {
 				break;
 			}
 
+			pageNo++;
 			page = new Page(mPdf, A4.PORTRAIT);
-			vanityLabel.drawOn(page);
 			table.setLocation(mHorizontalMargin, mVerticalMargin);
+
+			vanityLabel.drawOn(page);
+			writePageLabel(page, pageLabel, pageNo, pageTotal);
 		}
+
+		TextLine endLabel = new TextLine(mLabelFont, "SLUT");
+		endLabel.setLocation(mHorizontalMargin, tableBottom.getY() + endLabel.getHeight() + mInnerMargin);
+		endLabel.drawOn(page);
+	}
+
+	private void writePageLabel(Page page, TextLine pageLabel, int pageNo, int pageTotal) throws Exception {
+		pageLabel.setText(String.format(mContext.getString(R.string.document_export_page_format), pageNo, pageTotal));
+		pageLabel.setLocation(mPageWidth - mHorizontalMargin - pageLabel.getWidth(), mPageHeight - mFooterBottomMargin);
+		pageLabel.drawOn(page);
 	}
 
 	private float writeDocumentHeader(Page page, float centerOfPage) throws Exception {
@@ -169,7 +192,7 @@ public final class PdfGenerator {
 		recipientLabel.setLocation(centerOfPage + mInnerMargin, mVerticalMargin + mLabelFont.getHeight());
 		recipientLabel.drawOn(page);
 
-		final float addressBlockWidth = mPageWidth / 2.0f - mInnerMargin;
+		final float addressBlockWidth = mContentWidth / 2.0f - mInnerMargin;
 		final float labelHeight = senderLabel.getHeight();
 
 		TextBlock senderBlock = new TextBlock(mTextFont);
@@ -188,7 +211,7 @@ public final class PdfGenerator {
 
 		Line line = new Line(centerOfPage, mVerticalMargin, centerOfPage, headerBottom);
 		line.drawOn(page);
-		line = new Line(mHorizontalMargin, headerBottom, mHorizontalMargin + mPageWidth, headerBottom);
+		line = new Line(mHorizontalMargin, headerBottom, mHorizontalMargin + mContentWidth, headerBottom);
 		line.drawOn(page);
 
 		return headerBottom;
